ee386ac4a9c660e21b365b8060efe02e82d72808
Marin <220870464+MarinLCP@users.noreply.github.com>
2026-07-06 20:55:51 +0100

    j'ai mis le href dans le <script> au lieux de le mettre dans le style.css
    j'ai oublié le let devant le i = 0 de ma boucle

--- Script.js
+++ Script.js
@@ -2,13 +2,13 @@
 const divContainer = document.querySelector("#container");
 
 function createDivs () {
-    for(i = 0; i < 256; i++){
+    for(let i = 0; i < 256; i++){
     const square = document.createElement("div")
     const insereDansContainer = divContainer.appendChild(square);
 
-    square.classList = "square";
+    square.classList.add("square");
     }
 };
 
 
-createDivs
+createDivs()
