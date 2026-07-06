6be27d7f3322020bb0be1ce8c18ecff9d05b122b
Marin <220870464+MarinLCP@users.noreply.github.com>
2026-07-06 21:49:59 +0100

    j'ai mis un gap entre mes carré

--- Script.js
+++ Script.js
@@ -1,14 +1,18 @@
 
+// création de la grille avec des carré noir qui deviennent rouge au survol
 const divContainer = document.querySelector("#container");
 
 function createDivs () {
     for(let i = 0; i < 256; i++){
-    const square = document.createElement("div")
-    const insereDansContainer = divContainer.appendChild(square);
-
-    square.classList.add("square");
-    }
+        const square = document.createElement("div")
+        const insereDansContainer = divContainer.appendChild(square);
+        square.classList.add("square");
+        square.addEventListener("mouseover", () => {
+            square.classList.add("red")
+      });
+    };
 };
+
 
 
 createDivs()
