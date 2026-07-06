0911972ccab42db13306f475301b4f1cbc03899f
Marin <220870464+MarinLCP@users.noreply.github.com>
2026-07-06 22:32:43 +0100

    pas fier j'ai fini par faire gpt car fatigue. je vais faire une grosse lecture. mais en gros.
    j'ai une mauvaise compréhension du square.style.width = size + "px";
    enfaite ici avec les style on modifie directement le css

--- Script.js
+++ Script.js
@@ -1,18 +1,33 @@
-
-// création de la grille avec des carré noir qui deviennent rouge au survol
 const divContainer = document.querySelector("#container");
+const btnClickMe = document.querySelector("#btn");
 
-function createDivs () {
+function createDivs() {
     for(let i = 0; i < 256; i++){
-        const square = document.createElement("div")
-        const insereDansContainer = divContainer.appendChild(square);
+        const square = document.createElement("div");
+
         square.classList.add("square");
+
         square.addEventListener("mouseover", () => {
-            square.classList.add("red")
-      });
-    };
-};
+            square.classList.add("red");
+        });
 
+        divContainer.appendChild(square);
+    }
+}
 
+function changeSizeDivs() {
+    btnClickMe.addEventListener("click", () => {
+        const size = prompt("De quelle taille souhaitez-vous vos carrés en px ?");
+        const squares = document.querySelectorAll(".square");
+        if(size >= 100){console.log("to big sorryyyy")   
+        } else {
+            squares.forEach(square => {
+                square.style.width = size + "px";
+                square.style.height = size + "px";
+            })
+        };
+    });
+}
 
-createDivs()
+createDivs();
+changeSizeDivs();
